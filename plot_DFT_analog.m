7570d625b370657ca125745bf84d7e6e31bf6b51
Wilson Shao <wilsonwshao@gmail.com>
2016-02-07 15:56:29 -0800

    Correlation Function

--- plot_DFT_analog.m
+++ plot_DFT_analog.m
@@ -1,47 +1,61 @@
-function [ DFTx,Fd ] = plot_DFT_mag([w,x,y,z],fsample,figure_num )
+function plot_DFT_analog(w,x,y,z,fsample,figure_num)
 %Plot the DFT frequency spectrum (magnitude only in analog frequency axis)
 
 N = 250;
-Fd = 0:1/N: (N-1)/N;
+Fd = 0:1/N:(N-1)/N;
 %normalized value (n-1)/n
 
-DFTw = abs(fft(w));
+DFT = {w,x,y,z};
 
-DFTx = abs(fft(x));
+for n=1:numel(DFT)
+    subplot(2,2,n);
+    plot_helper(DFT)
+end
 
-DFTy = abs(fft(y));
+function plot_helper(DFT,fsample)
 
-DFTz = abs(fft(z));
+  if nargin < 4
+    DFT = abs(fft(DFT));
+    stem(Fd.*fsample,DFT./N,'.');
+    xlabel('Analog Frequency');
+    ylabel('Magnitude dB');
+    title('Magnitude vs. Analog Frequency');
+    grid on;
+  end
+
+end
 
 
-%{subplot(2,1,1)
-figure(figure_num);
-stem(Fd,DFTx./N,'.');
-xlabel('Digital Frequency')
-ylabel('Magnitude dB')
-title('Magnitude vs. Digital Frequency')
-axis([0 1 0 max(DFTx./N)]);
-grid on
-%}
+% subplot(2,1,1)
+% figure(figure_num);
+% stem(Fd.*fsample,DFT./N,'.');
+% xlabel('Analog Frequency')
+% ylabel('Magnitude dB')
+% title('Magnitude vs. Analog Frequency')
+% grid on
+%
+% subplot(2,1,2)
+% figure(figure_num+1);
+% stem(Fd.*fsample,DFT./N,'.');
+% xlabel('Analog Frequency')
+% ylabel('Magnitude dB')
+% title('Magnitude vs. Analog Frequency')
+% grid on
+%
+% subplot(2,1,3)
+% figure(figure_num+2);
+% stem(Fd.*fsample,DFT./N,'.');
+% xlabel('Analog Frequency')
+% ylabel('Magnitude dB')
+% title('Magnitude vs. Analog Frequency')
+% grid on
+%
+% subplot(2,1,4)
+% figure(figure_num+3);
+% stem(Fd.*fsample,DFT./N,'.');
+% xlabel('Analog Frequency')
+% ylabel('Magnitude dB')
+% title('Magnitude vs. Analog Frequency')
+% grid on
 
-subplot(2,1,1)
-figure(figure_num);
-stem(Fd.*fsample,DFTx./N,'.');
-xlabel('Analog Frequency')
-ylabel('Magnitude dB')
-title('Magnitude vs. Analog Frequency')
-
-subplot(2,1,2)
-figure(figure_num+1);
-stem(Fd.*fsample,DFTx./N,'.');
-xlabel('Analog Frequency')
-ylabel('Magnitude dB')
-title('Magnitude vs. Analog Frequency')
-
-subplot(2,1,3)
-figure(figure_num+1);
-stem(Fd.*fsample,DFTx./N,'.');
-xlabel('Analog Frequency')
-ylabel('Magnitude dB')
-title('Magnitude vs. Analog Frequency')
 end
